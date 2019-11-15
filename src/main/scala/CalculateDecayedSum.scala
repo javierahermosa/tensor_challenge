87697226d23b8e3337082542809af3efd7036d49
Javiera Guedes <javieraguedes@Javieras-MacBook-Pro.local>
2019-11-15 11:35:58 +0100

    added final report structure

--- src/main/scala/CalculateDecayedSum.scala
+++ src/main/scala/CalculateDecayedSum.scala
@@ -3,45 +3,46 @@ import org.apache.spark.sql.expressions.{MutableAggregationBuffer, UserDefinedAg
 import org.apache.spark.sql.types._
 
 
-class CalculateDecayedSum(hl: Array[Double]) extends UserDefinedAggregateFunction{
+class CalculateDecayedSum extends UserDefinedAggregateFunction{
 
   // Input schema for this UserDefinedAggregateFunction
   override def inputSchema: StructType =
     StructType(
-        StructField("time_delta", DoubleType) ::
-        StructField("volume", IntegerType) ::
+      StructField("time_delta", DoubleType) ::
+        StructField("bid_volumes", ArrayType(IntegerType)) ::
+        StructField("ask_volumes", ArrayType(IntegerType)) ::
+        StructField("half_life", DoubleType) ::
         StructField("status", StringType) :: Nil)
 
   // Schema for the parameters that will be used internally to buffer temporary values
   override def bufferSchema: StructType = StructType(
-    StructField("decayed_sum_hl_1", DoubleType)::
-      StructField("decayed_sum_hl_2", DoubleType)::
-      StructField("decayed_sum_hl_3", DoubleType) :: Nil
+    StructField("bid_decayed_sums", ArrayType(DoubleType)) ::
+      StructField("ask_decayed_sums", ArrayType(DoubleType)):: Nil
   )
 
   // The data type returned by this UserDefinedAggregateFunction.
   override def dataType: DataType = StructType(
-    Seq(StructField("decayed_sum_hl_1", DoubleType),
-        StructField("decayed_sum_hl_2", DoubleType),
-        StructField("decayed_sum_hl_3", DoubleType)))
+    Seq(StructField("bid_decayed_sums", ArrayType(DoubleType)),
+        StructField("ask_decayed_sums", ArrayType(DoubleType))))
 
   // Whether this UDAF is deterministic or not. In this case, it is
   override def deterministic: Boolean = true
 
   // Initial values for the temporary values declared above
   override def initialize(buffer: MutableAggregationBuffer): Unit = {
-    buffer(0) = 0.0
-    buffer(1) = 0.0
-    buffer(2) = 0.0
+    buffer(0) = Seq(0.0, 0.0, 0.0, 0.0, 0.0)
+    buffer(1) = Seq(0.0, 0.0, 0.0, 0.0, 0.0)
   }
 
   // Function to calculate the decayed sum
-  private def decayedSum(etds_prev: Double, timeDelta: Double, volume:Int, hl: Double, status: String): Double = {
+  private def decayedSum(etds_prev: Seq[Double], timeDelta: Double, volumes:Seq[Int], hl: Double, status: String): Seq[Double] = {
     val factor = math.pow(2, -1.0 * timeDelta / hl)
-    if(status == "reset_time" | status == "reset_tick") {
-      0.0
-    } else {
-      etds_prev * factor + volume
+    volumes.zip(etds_prev).map{ case (volume, etds_prev) =>
+      if(status == "reset_time" | status == "reset_tick") {
+        0.0
+      } else {
+        etds_prev * factor + volume
+      }
     }
   }
 
@@ -49,12 +50,16 @@ class CalculateDecayedSum(hl: Array[Double]) extends UserDefinedAggregateFunctio
   override def update(buffer: MutableAggregationBuffer, input: Row): Unit = {
 
     val timeDelta = input.getDouble(0)
-    val volume = if(input.isNullAt(1)) 0 else input.getInt(1)
-    val status = input.getString(2)
+    val bid_volumes = input.getAs[Seq[Int]](1)
+    val ask_volumes = if (input.getAs[Seq[Int]](2).isEmpty) Seq(0, 0, 0, 0, 0) else input.getAs[Seq[Int]](2)
+    val hl = input.getDouble(3)
+    val status = input.getString(4)
+    val etds_bid_prev = buffer.getAs[Seq[Double]](0)
+    val etds_ask_prev = buffer.getAs[Seq[Double]](1)
 
-    buffer(0) = decayedSum(buffer.getDouble(0), timeDelta, volume, hl(0) , status)
-    buffer(1) = decayedSum(buffer.getDouble(1), timeDelta, volume, hl(1) , status)
-    buffer(2) = decayedSum(buffer.getDouble(2), timeDelta, volume, hl(2) , status)
+    buffer(0) = decayedSum(etds_bid_prev, timeDelta, bid_volumes, hl, status)
+    buffer(1) = decayedSum(etds_ask_prev, timeDelta, ask_volumes, hl, status)
+
   }
 
   // Function used to merge two objects. In this case, it is not necessary to define this method since
@@ -63,7 +68,7 @@ class CalculateDecayedSum(hl: Array[Double]) extends UserDefinedAggregateFunctio
 
   // Function returns this
   override def evaluate(buffer: Row): Any = {
-    (buffer.getDouble(0), buffer.getDouble(1), buffer.getDouble(2))
+    (buffer.getList(0), buffer.getList(1))
   }
 
 }
--- src/main/scala/Tensor.scala
+++ src/main/scala/Tensor.scala
@@ -3,7 +3,7 @@ import org.apache.spark.sql.{Row, SparkSession}
 import org.apache.spark.sql.expressions.{UserDefinedFunction, Window}
 import org.apache.spark.sql.functions._
 
-class Tensor(resetTicks: List[Int], resetTimes: List[Int]) extends Serializable {
+class Tensor(resetTicks: List[Int], resetTimes: List[Double]) extends Serializable {
   @transient lazy val logger: Logger = Logger.getLogger(getClass.getName)
 
   def say_hello(arg: String): Unit = {
@@ -13,12 +13,13 @@ class Tensor(resetTicks: List[Int], resetTimes: List[Int]) extends Serializable 
 
   def resetSum: UserDefinedFunction = udf(
     (tick: Long, tick_post: Long, dt: Double, dt_prev: Double, dt_post: Double) => {
-    (tick, dt) match {
-      case (x, _) if resetTicks.contains(x) => "reset_tick"
-      case (_, y) if resetTimes.exists(rt => (y >= rt) && (dt_prev < rt)) => "reset_time"
-      case (_, y) if resetTicks.contains(tick_post) |  resetTimes.exists(rt => (y < rt) && (dt_post > rt)) => "report"
-      case (x, _) if x == 1.0 => ""
-      case (_,_) => ""
+    (tick, dt, tick_post) match {
+      case (x, _, _) if resetTicks.contains(x) => "reset_tick"
+      case (_, y, _) if resetTimes.exists(rt => (y >= rt) && (dt_prev < rt)) => "reset_time"
+      case (_, _, z) if resetTicks.contains(z) => "report_tick_reset"
+      case (_, y, _) if resetTimes.exists(rt => (y < rt) && (dt_post > rt)) => "report_time_reset"
+      case (x, _, _) if x == 1.0 => ""
+      case (_,_, _) => ""
     }
   })
 }
@@ -36,11 +37,10 @@ object Tensor extends Serializable {
 
     val path = "src/main/resources/challenge.json"
     val data = IO.jsonToDataFrame(spark, path)
-    val data2 = IO.jsonToDataFrame3(spark, path)
 
-    val hls = Array(1e6/1e9, 1e9/1e9, 1e12/1e9)
+    val hls = Array(1e6, 1e9, 1e12)
     val resetTicks = List(1000, 1000000)
-    val resetTimes = List(1, 60, 3600)
+    val resetTimes = List(1, 60, 3600).map(_ * 1e9) // seconds to ns
 
     val Row(minTime: Long) = data.agg(min("time")).head
 
@@ -48,49 +48,27 @@ object Tensor extends Serializable {
     val w = Window.orderBy("time")
     val w_res = Window.partitionBy("half_life").orderBy("time")
 
-    val calculateDecayedSum = new CalculateDecayedSum(hls)
-    val calculateDecayedSum2 = new CalculateDecayedSum2
+    val calculateDecayedSum2 = new CalculateDecayedSum
     val tensor = new Tensor(resetTicks, resetTimes)
 
-    val dataPrep = data
+    val results = data
       .withColumn("tick", row_number().over(w))
       .withColumn("time_prev", lag("time", 1, minTime).over(w))
-      .withColumn("time_delta_seconds", (col("time") - col("time_prev"))/1e9)
+      .withColumn("time_delta_ns", (col("time") - col("time_prev")))
       //.withColumn("time_elapsed_seconds", sum("time_delta_seconds").over(w))
-      .withColumn("time_elapsed_seconds", (col("time") - minTime)/1e9)
-      .withColumn("time_elapsed_seconds_prev", lag("time_elapsed_seconds", 1).over(w))
-      .withColumn("time_elapsed_seconds_post", lead("time_elapsed_seconds", 1).over(w))
+      .withColumn("time_elapsed_ns", (col("time") - minTime))
+      .withColumn("time_elapsed_ns_prev", lag("time_elapsed_ns", 1).over(w))
+      .withColumn("time_elapsed_ns_post", lead("time_elapsed_ns", 1).over(w))
       .withColumn("tick_post", lead("tick", 1).over(w))
-      .withColumn("status", tensor.resetSum($"tick", $"tick_post", $"time_elapsed_seconds", $"time_elapsed_seconds_prev", $"time_elapsed_seconds_post"))
-      //.select($"time_elapsed_seconds", $"bid_volumes", $"ask_volumes", $"status")
-      .withColumn("v1_sum", calculateDecayedSum($"time_delta_seconds", $"bid_v1", $"status").over(w))
-      .select("v1_sum")
-      .orderBy($"time_elapsed_seconds")
-      .filter($"status"==="report")
-      //.withColumn("volume1_prev", lag("bid_v1", 1).over(w))
+      .withColumn("status", tensor.resetSum($"tick", $"tick_post", $"time_elapsed_ns", $"time_elapsed_ns_prev", $"time_elapsed_ns_post"))
+      //.withColumn("hls", (lit(hls)))
+      .withColumn("half_life", explode(lit(hls)))
+      .withColumn("result", calculateDecayedSum2($"time_delta_ns", $"bid_volumes", $"ask_volumes", $"half_life", $"status").over(w_res))
+      .select($"time", $"tick", $"half_life", $"bid_volumes", $"ask_volumes", $"time_elapsed_ns", $"result.bid_decayed_sums", $"result.ask_decayed_sums", $"status")
+      .filter($"status".contains("report"))
+      .orderBy($"time", $"half_life")
 
-    val dataPrep2 = data2
-      .withColumn("tick", row_number().over(w))
-      .withColumn("time_prev", lag("time", 1, minTime).over(w))
-      .withColumn("time_delta_seconds", (col("time") - col("time_prev"))/1e9)
-      //.withColumn("time_elapsed_seconds", sum("time_delta_seconds").over(w))
-      .withColumn("time_elapsed_seconds", (col("time") - minTime)/1e9)
-      .withColumn("time_elapsed_seconds_prev", lag("time_elapsed_seconds", 1).over(w))
-      .withColumn("time_elapsed_seconds_post", lead("time_elapsed_seconds", 1).over(w))
-      .withColumn("tick_post", lead("tick", 1).over(w))
-      .withColumn("status", tensor.resetSum($"tick", $"tick_post", $"time_elapsed_seconds", $"time_elapsed_seconds_prev", $"time_elapsed_seconds_post"))
-      .withColumn("hls", (lit(hls)))
-      .withColumn("half_life", explode($"hls"))
-      .withColumn("result", calculateDecayedSum2($"time_delta_seconds", $"bid_volumes", $"ask_volumes", $"half_life", $"status").over(w_res))
-      .select($"time", $"tick", $"half_life", $"bid_volumes", $"ask_volumes", $"time_elapsed_seconds", $"result.bid_decayed_sums", $"result.ask_decayed_sums")
-    dataPrep2.show(100, false)
-    //dataPrep.show(100, false)
-    //data.orderBy($"time").show(100, false)
-
-
-    //val d3 = IO.jsonToDataFrame3(spark, path)
-    //d3.show(100, false)
-    //println(d3.count)
+    results.show(100, false)
     sc.stop()
   }
 }
